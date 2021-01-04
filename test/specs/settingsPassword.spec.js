a87e047ed2ec5813f7f2eec17b4a86fd8d4b7a5f
Elena-Sergienko <elena.serg.us@gmail.com>
2021-01-03 21:45:28 -0800

    Upd: settingsPassword.spec.js, settingsPassword.page.js

--- test/specs/settingsPassword.spec.js
+++ test/specs/settingsPassword.spec.js
@@ -1,36 +1,71 @@
 import LoginPage from "../pageobjects/login.page";
-import ProfilePage from "../pageobjects/profile.page";
-import SettingsProfilePage from "../pageobjects/settings/settingsProfile.page"
 import SettingsPasswordPage from "../pageobjects/settings/settingsPassword.page"
-import SettingsLinksPage from "../pageobjects/settings/settingsLinks.page"
 import MenuPage from "../pageobjects/menu.page"
 import MainMenu from "../pageobjects/menu.page";
 
 
-describe('Settings Password', () => {
+describe('TS: SETTINGS PASSWORD', () => {
 
-    const inpOldPassword = "111111";
-    const inpNewPassword = "test222222";
-    const inpConfirmNew = "test222222";
+
+    const inpData = {
+        oldPassword: "111111",
+        newPassword: "test222222",
+        confirmNew: "test222222",
+        email: "test@account.com",
+    }
+
+    const expData = {
+        alertText: "New passwords do not match.",
+        errorText: "User settings update password. Fail"
+    }
+
 
     before(() => {
         browser.maximizeWindow();
-        LoginPage.login("test@account.com", inpOldPassword);
+        LoginPage.login(inpData.email, inpData.oldPassword);
     })
 
-    xit('Edit Google Doc resume link', () => { // кнопка не становится активной - баг??
+    beforeEach(() => {
         MenuPage.goToSettingsPassword();
+    })
 
-        SettingsPasswordPage.inputFieldOldPassword.setValue(inpOldPassword);
-        SettingsPasswordPage.inputFieldNewPassword.setValue(inpNewPassword);
-        SettingsPasswordPage.inputFieldConfirm.setValue(inpConfirmNew);
+    it('Verify that it is possible to update the password', () => {
+        SettingsPasswordPage.inputFieldOldPassword.setValue(inpData.oldPassword);
+        SettingsPasswordPage.inputFieldNewPassword.setValue(inpData.newPassword);
+        SettingsPasswordPage.inputFieldConfirm.setValue(inpData.confirmNew);
         SettingsPasswordPage.updatePasswordBtn.waitForClickable();
         SettingsPasswordPage.updatePasswordBtn.click();
 
         MenuPage.goToLogout();
-        LoginPage.login("test@account.com", inpNewPassword);
+        LoginPage.login(inpData.email, inpData.newPassword);
 
         expect(MainMenu.topMenu).toBeExisting();
     })
 
+    it('TC: Verify that Error handling works correctly', () => {
+        SettingsPasswordPage.inputFieldOldPassword.setValue(inpData.newPassword);
+        SettingsPasswordPage.inputFieldNewPassword.setValue(inpData.oldPassword);
+        SettingsPasswordPage.inputFieldConfirm.setValue(inpData.newPassword);
+
+        expect(SettingsPasswordPage.alertPasswordDoNotMatch).toHaveText(expData.alertText);
+
+        SettingsPasswordPage.updatePasswordBtn.click();
+        expect(SettingsPasswordPage.errorMessage).toHaveText(expData.errorText);
+    })
+
+    after(() => {
+        browser.refresh()
+        MenuPage.goToSettingsPassword();
+
+        SettingsPasswordPage.inputFieldOldPassword.setValue(inpData.newPassword);
+        SettingsPasswordPage.inputFieldNewPassword.setValue(inpData.oldPassword);
+        SettingsPasswordPage.inputFieldConfirm.setValue(inpData.oldPassword);
+        SettingsPasswordPage.updatePasswordBtn.waitForClickable();
+        SettingsPasswordPage.updatePasswordBtn.click();
+
+        MenuPage.goToLogout();
+        LoginPage.login(inpData.email, inpData.oldPassword);
+
+        expect(MainMenu.topMenu).toBeExisting();
+    })
 })

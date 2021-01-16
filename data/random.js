bc68872a92650e41790b37c29bd61ab97173c02c
Elena-Sergienko <elena.serg.us@gmail.com>
2021-01-15 21:39:11 -0800

    Upd config

---
+++ data/random.js
@@ -0,0 +1,8 @@
+// import faker from 'faker';
+//
+// const randomUser = {
+//     firstName: faker.name.firstName(),
+//     lastName: faker.name.lastName(),
+//     email: faker.internet.email(),
+//
+// }
